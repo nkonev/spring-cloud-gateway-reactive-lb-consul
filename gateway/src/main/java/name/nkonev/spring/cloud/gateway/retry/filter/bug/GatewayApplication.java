9c329fd0e1ce0694728dc6f051990af5072792e0
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-01 01:51:48 +0300

    reproduced via programmatic configuration

--- gateway/src/main/java/name/nkonev/spring/cloud/gateway/retry/filter/bug/GatewayApplication.java
+++ gateway/src/main/java/name/nkonev/spring/cloud/gateway/retry/filter/bug/GatewayApplication.java
@@ -2,8 +2,11 @@ package name.nkonev.spring.cloud.gateway.retry.filter.bug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
-// curl -v -X POST -d 'foo=bar' -w "\n"  http://127.0.0.1:8282/post
+// while true; do curl  http://localhost:8282/foo; sleep 1; done
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -11,4 +14,12 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/foo/**")
+                        .uri("lb://foo")
+                        .id("foo"))
+                .build();
+    }
 }
