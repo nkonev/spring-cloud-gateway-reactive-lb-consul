9895d3b11e5f31cb7bf176acd4204f993340e44d
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-01 15:10:34 +0300

    cleanup

--- gateway/src/main/java/name/nkonev/spring/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/name/nkonev/spring/cloud/gateway/GatewayApplication.java
@@ -1,9 +1,8 @@
-package name.nkonev.spring.cloud.gateway.retry.filter.bug;
+package name.nkonev.spring.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// curl -v -X POST -d 'foo=bar' -w "\n"  http://127.0.0.1:8282/post
 @SpringBootApplication
 public class GatewayApplication {
 
