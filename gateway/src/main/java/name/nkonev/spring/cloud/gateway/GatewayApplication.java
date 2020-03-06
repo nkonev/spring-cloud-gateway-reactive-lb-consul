dca058f519854ee263d19c43a96ae10114d71c24
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-06 03:07:34 +0300

    fix issue with per-cb customizer

--- gateway/src/main/java/name/nkonev/spring/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/name/nkonev/spring/cloud/gateway/GatewayApplication.java
@@ -2,17 +2,16 @@ package name.nkonev.spring.cloud.gateway;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import javax.annotation.PostConstruct;
+
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -23,21 +22,17 @@ public class GatewayApplication {
 
     public static final String TEST_CIRCUIT_BREAKER = "testCircuitBreaker";
 
-    @Autowired
-    private ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory;
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @PostConstruct
-    public void pc(){
-        reactiveResilience4JCircuitBreakerFactory.configureDefault(id -> {
-            TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom().timeoutDuration(Duration.of(7, ChronoUnit.SECONDS)).build();
-            Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration configuration = new Resilience4JConfigBuilder(id).circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                    .timeLimiterConfig(timeLimiterConfig).build();
-            return configuration;
-        });
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> testCircuitBreakerCustomizer() {
+        return factory -> {
+            factory.configure(builder -> builder
+                    .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.of(7, ChronoUnit.SECONDS)).build())
+                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults()), TEST_CIRCUIT_BREAKER);
+        };
     }
 
     @Bean
