fe055065d14525c6912ae80eb35b4e7c492bc5c9
Nikita Konev <nikit.cpp@yandex.ru>
2021-12-29 04:24:39 +0300

    initial h2c example

---
+++ foo-microservice/src/main/java/name/nkonev/foo/microservice/H2CServer.java
@@ -0,0 +1,22 @@
+package name.nkonev.foo.microservice;
+
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.server.HttpServer;
+
+public class H2CServer {
+    // curl --http2-prior-knowledge  'http://localhost:8088'
+    public static void main(String[] args) {
+        DisposableServer server =
+                HttpServer.create()
+                        .port(8088)
+                        .protocol(HttpProtocol.H2C)
+                        .handle((request, response) -> response.sendString(Mono.just("hello")))
+                        .bindNow();
+
+        server.onDispose()
+                .block();
+
+    }
+}
